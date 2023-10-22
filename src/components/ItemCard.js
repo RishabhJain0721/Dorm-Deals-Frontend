9a49848321f4ee07832d9eae8786c217d68e80ac
ishitaraina1807 <ishitaraina18@gmail.com>
2023-10-22 16:25:31 +0530

    improved UI of cards

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -15,26 +15,35 @@ const ItemCard = (props) => {
   return (
     <div
       onClick={moreInfo}
-      className="bg-gray-100 hover:cursor-pointer shadow-lg rounded-lg overflow-hidden flex flex-col justify-center items-center hover:translate-y-[-5px] transition duration-300 ease-in-out w-50 p-2 m-5 mx-10"
+      className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-4"
     >
-      {/* Product Image(First image from the three uploaded ones) */}
-      <img
-        src={src}
-        alt={props.rest.itemName}
-        className="w-full h-32 object-cover"
-      />
-
-      {/* Product Details */}
-      <div className="p-4 text-center">
-        {/* Product Name */}
-        <h2 className="text-gray-800 text-xl font-semibold">
-          {props.rest.itemName}
-        </h2>
-        <hr />
-
-        {/* Product Price */}
-        <p className="mt-2 text-red-500">Rs. {props.rest.itemCost}</p>
-        <p>Provider : {props.rest.userName} </p>
+      <div className="card flex flex-col justify-center p-10 bg-white rounded-lg shadow-2xl h-full">
+        <div className="prod-img">
+          <img
+            src={src}
+            alt={props.rest.itemName}
+            className="w-full h-40 sm:h-48 md:h-56 object-cover object-center"
+          />
+        </div>
+        <div className="prod-title">
+          <p className="text-xl sm:text-3xl uppercase text-gray-900 font-bold">
+            {props.rest.itemName}
+          </p>
+          <p className="uppercase text-sm sm:text-base text-gray-400">
+            The best {props.rest.itemName.toLowerCase()} in the marketplace
+          </p>
+        </div>
+        <div className="prod-info grid gap-6">
+          <p className="font-bold text-xl sm:text-2xl">Rs. {props.rest.itemCost}</p>
+          <div className="flex justify-center items-center">
+            <button
+              className="px-6 py-2 bg-gray-900 text-white transition ease-in duration-200 uppercase rounded-full hover:bg-red-500 hover:text-white border-2 focus:outline-none"
+              onClick={() => alert("Add to cart clicked")}
+            >
+              Buy Now
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
