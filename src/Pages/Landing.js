46940a19623525b7c2b6fac97f7f75186b5ddc57
Ritik Dangi <ritikdangi445566@gmail.com>
2024-03-20 00:15:52 +0530

    updated landing page to look more user interactive and for good visibility .

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { WhiteButton, YellowButton } from "../components/Buttons";
-
+    
 const LandingPage = () => {
   return (
-    <div className="bg-blue-500 min-h-screen flex flex-col justify-center items-center text-center px-4">
-      <div className="text-white text-3xl md:text-5xl font-semibold mb-6">
+    <div className=" bg-[url('C:\Users\welcome\OneDrive\Desktop\drom\Dorm-Deals-Frontend\src\Images\landingimg.jpg')]  bg-cover min-h-screen  flex flex-col justify-center items-center text-center px-4">
+        <div className=" bg-white bg-opacity-30 w-45 m-30 p-8 py-14 rounded-lg  shadow-cyan-500/50 shadow-lg">
+        <div className="text-white text-3xl md:text-5xl font-semibold mb-6">
         Welcome to College Marketplace
       </div>
       <div className="text-white md:text-lg mb-8">
         Buy and sell goods within your college community.
       </div>
       <div className="space-x-4">
-        <YellowButton val="Log In" navigation="/login" />
+        <YellowButton val="Log In" navigation="/login"  />
         <WhiteButton val="Sign Up" navigation="/signup" />
       </div>
+        </div>
+     
     </div>
   );
 };
