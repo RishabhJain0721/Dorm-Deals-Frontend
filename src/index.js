89f5ef067fd6ebbd1ef4402c1dc17da0d8e533c6
Rajan21252125 <99201190+Rajan21252125@users.noreply.github.com>
2024-03-21 09:25:25 +0530

    merge login aignup && toggle show hide password && added toastify

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { AuthContextProvider } from "./Contexts/AuthContext";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <AuthContextProvider>
     <React.StrictMode>
       <App />
+      <ToastContainer position="top-center" autoClose={3000} hideProgressBar={false} newestOnTop closeOnClick rtl={false} pauseOnFocusLoss={false} draggable pauseOnHover
+      />
     </React.StrictMode>
   </AuthContextProvider>
 );
