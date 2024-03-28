6429cdc9caa94cb8c611933f79783973488caa4d
Ritik Dangi <ritikdangi445566@gmail.com>
2024-03-28 09:17:52 +0530

    bg image resolutionand size rreduced

--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { WhiteButton, YellowButton } from "../components/Buttons";
-import {LazyLoadImage} from 'react-lazy-load-image-component';
+
 const LandingPage = () => {
   return (
-    <div className=" bg-[url('C:\Users\welcome\OneDrive\Desktop\drom\Dorm-Deals-Frontend\src\Images\landingimg1.jpg')]  bg-cover min-h-screen bg-center-cover flex flex-col justify-center items-center text-center px-4">
+    <div className=" bg-[url('C:\Users\welcome\OneDrive\Desktop\drom\Dorm-Deals-Frontend\src\Images\landingimg123.jpg')]  bg-cover min-h-screen bg-center-cover flex flex-col justify-center items-center text-center px-4">
         <div className=" bg-white bg-opacity-30 w-45 m-30 p-8 py-14 rounded-lg  shadow-cyan-500/50 shadow-lg">
         <div className="text-white text-3xl md:text-5xl font-semibold mb-6">
         Welcome to College Marketplace
